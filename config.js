c169628a772e8d71719326e42c8eda0013ee60f6
vietduccelo <85992406+vietduccelo@users.noreply.github.com>
2021-06-16 16:43:23 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,10 @@
 const CONFIG = {
     introTitle: 'Bé con nah !',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introDesc: `grrr Vũ Trụ thông báo đây là thông điệp từ người yêu của em `,
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
+    btnYes: 'Rõ dàng nà gấc thích anh ỏ <33',
     btnNo: 'ứ ừ , khônggg :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
